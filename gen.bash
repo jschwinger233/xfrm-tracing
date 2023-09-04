cfc8b1c033e7db4c5841da0b7b04eeca65643173
Gray Liang <gray.liang@isovalent.com>
2023-09-04 17:05:54 +0800

    Update Dockerfile for cloud use

--- gen.bash
+++
@@ -1,4 +0,0 @@
-head=$(cat /proc/kallsyms | awk '$2 ~ /t/ {print $1}' | sort  | head -1)
-tail=$(cat /proc/kallsyms | awk '$2 ~ /t/ {print $1}' | sort  | tail -1)
-diff=$((0x$tail - 0x$head))
-gdb -ex "x/${diff}i 0x$head" -ex q /proc/kcore /proc/kcore | python3 filter_xfrm_inc_asm.py  > xfrm_inc_ctx.yaml
